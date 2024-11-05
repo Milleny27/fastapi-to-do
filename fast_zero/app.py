4fc217b41e0792ee7e7b19672c3d029604d88c2c
Milleny Ellen Jodas Ferreira <ellenmilleny@gmail.com>
2024-11-05 16:11:16 -0300

    feat: implement  endpoint olamundo & test: add test for endpoint olamundo

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, world!'}
+
+
+@app.get('/olamundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def exercicio_dois():
+    return """
+        <html>
+            <head>
+                <title>Exercício Aula 02</title>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+            </body>
+        </html>
+        """
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,3 +15,15 @@ def test_read_root_deve_retornar_OK_e_ola_mundo():
     # assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, world!'}
+
+
+def test_exercicio_dois_deve_retornar_OK_e_ola_mundo_no_htlm():
+    # arrange
+    client = TestClient(app)
+
+    # act
+    response = client.get('/olamundo')
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello, World!</h1>' in response.text
